e7f574027537d114785f18ef78e5d25cb7b67f2a
oooscarrr <60665133+oooscarrr@users.noreply.github.com>
2021-04-16 00:54:36 +0800

    Add files via upload

---
+++ MyProcessingAssignment/src/ruizhizi_xue_oscar_assignment6/edu/nyu/cs/yx2021/rx513/SmallBone.java
@@ -0,0 +1,70 @@
+package ruizhizi_xue_oscar_assignment6.edu.nyu.cs.yx2021.rx513;
+
+import processing.core.PImage;
+
+/**
+ * This class represents the small bones displaying the current score
+ * @author Vincent and Oscar
+ * @version 0.1
+ */
+
+public class SmallBone {
+	//Hold a reference to the PlayGame object
+	PlayGame app;
+	//To reference the SmallBone image
+	private final static String SMALLBONE_IMAGE_PATH = "SmallBone.png";
+	private PImage img;
+	//position
+	private int x,y; 
+	//The height and width of the SmallBone.
+	private final static int HEIGHT = 20;
+	private final static int WIDTH = 50;
+	/**
+	 * Constructor
+	 * @param app PlayGame
+	 * @param x position
+	 * @param y position
+	 */
+	public SmallBone(PlayGame app, int x, int y) {
+		this.app=app;
+		this.setX(x);
+		this.setY(y);
+		this.img = app.loadImage(SmallBone.SMALLBONE_IMAGE_PATH);
+	}
+	/**
+	 * Setters
+	 */
+	public void setX(int x) {
+		this.x=x;
+	}
+	public void setY(int y) {
+		this.y=y;
+	}
+	/**
+	 * Getters
+	 */
+	public int getX() {
+		int x = this.x;
+		return x;
+	}
+	public int getY() {
+		int y = this.y;
+		return y;
+	}
+	public static int getHeight() {
+		return SmallBone.HEIGHT;
+	}
+	public static int getWidth() {
+		return SmallBone.WIDTH;
+	}
+	/**
+	 * Methods
+	 * Actions SmallBone could do.
+	 */
+	public void gainScore() {
+		this.app.getSmallBones().add(this);
+	}
+	public void draw() {
+		this.app.image(this.img, this.x, this.y);
+	}
+}
